cfd857bda3ca30ae156c59ff7474cb4848e7f723
Jakub Stawiszyński <kubastyk@wp.pl>
2017-06-25 12:51:43 +0200

    refactorization

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -3,6 +3,7 @@ package edu.iis.mto.serverloadbalancer;
 
 public class Server {
 
+	private static final double _MAXIMUM_LOAD = 100.0d;
 	public double currentLoadPecentage;
 	public int capacity;
 
@@ -13,6 +14,10 @@ public class Server {
 	public Server(int capacity) {
 		super();
 		this.capacity = capacity;
+	}
+
+	public void addVm(Vm vm) {
+		currentLoadPecentage = (double)vm.size / (double)capacity * _MAXIMUM_LOAD;
 	}
 
 }
