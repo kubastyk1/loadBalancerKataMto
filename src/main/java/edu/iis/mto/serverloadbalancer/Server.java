0744a0bdf086b082aff799ff929ffad407bd6d25
Jakub Stawiszyński <kubastyk@wp.pl>
2017-06-25 12:04:11 +0200

    filling server with vm implemented

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,10 +1,17 @@
 package edu.iis.mto.serverloadbalancer;
 
+import org.hamcrest.Matcher;
+
 public class Server {
 
 	public double currentLoadPecentage;
 
 	public Server(int capacity) {
+	}
+
+	public boolean contains(Vm theVm) {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 }
